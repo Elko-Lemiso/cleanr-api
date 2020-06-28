d2b22611261337f34f0558f828e49def66c735b7
Nardo Kraaijeveld <nardo.kra@gmail.com>
2020-06-28 17:09:07 +0200

    sign up roughly setup, not tested yet

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
+require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
+var mongoose = require('mongoose');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var hbs = require('hbs');
 var cors = require('cors');
 var connectionPassword = process.env.CONNECTIONPASSWORD;
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var bodyParser   = require('body-parser');
+var secret = process.env.SECRET;
+var session    = require("express-session");
+// var MongoStore = require("connect-mongo")(session);
 
 mongoose
-  .connect(`${connectionPassword}`, {useNewUrlParser: true})
+  .connect(`${connectionPassword}`, {
+    useNewUrlParser: true,  
+    useCreateIndex: true,
+    useUnifiedTopology: true
+  })
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -33,11 +39,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-hbs.registerPartials(__dirname + '/views/partials');
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users/signup'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
