217bee185a427650da26e5970ca5dfd3838ae5c8
Elko Lemiso <elkovandongen@gmail.com>
2020-06-29 17:05:12 +0200

    Rwady to pull

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var hbs = require('hbs');
 var cors = require('cors')
 
 var indexRouter = require('./routes/index');
@@ -16,15 +15,12 @@ app.use(cors({
 }))
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-hbs.registerPartials(__dirname + '/views/partials');
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
