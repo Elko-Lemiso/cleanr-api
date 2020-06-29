e7f25d266c070f616079c1776c2324f41e1fe39f
Elko Lemiso <elkovandongen@gmail.com>
2020-06-29 21:44:16 +0200

    Styling

---
+++ routes/users/login.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router  = express.Router();
+const User = require('../../models/User');
+const bcrypt = require('bcrypt');
+
+router.post("/login", (req, res, next)=>{
+  const email = req.body.email;
+  const password = req.body.password;
+  if (email === "" || password === "") {
+    res
+    .status(401)
+    .json({ errorMessage: 'Unauthorized, nothing passed into fields' });
+      return;
+    }
+  User.findOne({"email": email})
+  .then(user =>{
+        if (!user) {
+          res.json({errorMessage: "The email doesn't exist." });
+          return;
+        }
+        if(bcrypt.compareSync(password, user.password)){
+          req.session.user = user;
+          console.log(req.session);
+          let sessionData = req.session.user
+          res.status(200).json({sessionData});
+        }else{
+          res.status(401).json({errorMessage: 'Unauthorized, wrong password'});
+        }
+  })
+  .catch(error => {
+      next(error);
+  })
+})
+module.exports = router;
