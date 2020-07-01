deb4186919e119dd4d196f81b7cab91357469936
Nardo Kraaijeveld <nardo.kra@gmail.com>
2020-07-01 09:10:42 +0200

    reduced session data for login

--- routes/users/login.js
+++ routes/users/login.js
@@ -19,9 +19,9 @@ router.post("/login", (req, res, next)=>{
           return;
         }
         if(bcrypt.compareSync(password, user.password)){
-          req.session.user = user;
-          console.log(req.session);
-          let sessionData = req.session.user
+          let {email, firstname, lastname, id, userType} = user;
+          let sessionData = {email, firstname, lastname, id, userType};
+          req.session.user = sessionData;
           res.status(200).json({sessionData});
         }else{
           res.status(401).json({errorMessage: 'Unauthorized, wrong password'});
