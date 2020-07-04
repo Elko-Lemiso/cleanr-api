1fe6c17199a0b8601fa269aea2bbf6818ee1315f
Elko Lemiso <elkovandongen@gmail.com>
2020-07-04 11:20:50 +0200

    Push

--- app.js
+++ app.js
@@ -54,8 +54,8 @@ app.use('/users', require('./routes/users/login'));
 app.use('/users', require('./routes/users/logout'));
 app.use('/users', require('./routes/users/userData'))
 app.use('/users', require('./routes/users/editProfile'));
-app.use(('/jobs'), require('./routes/jobs/postJob'));
-app.use(('/jobs'), require('./routes/jobs/editJob'));
+app.use('/jobs', require('./routes/jobs/postJob'));
+app.use('/jobs', require('./routes/jobs/editJob'));
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- routes/jobs/editJob.js
+++ routes/jobs/editJob.js
@@ -4,14 +4,13 @@ const User = require('../../models/User');
 const uploadCloud = require('../../config/cloudinary');
 const Job = require('../../models/Job');
 
-router.post('/editJob', (req, res, next)=>{
+router.post('/editJob', uploadCloud.array('images'), (req, res, next)=>{
   debugger
 
   const findJob = req.body.jobId;
-
-
-  const change = {
-    title: req.body.title,
+  
+  let change = {
+        title: req.body.title,
         description: req.body.description,
         rate: req.body.rate,
         dueDate: req.body.dueDate,
@@ -23,19 +22,14 @@ router.post('/editJob', (req, res, next)=>{
           city: req.body.address.city,
           long: req.body.address.long,
           lat: req.body.address.lat,
-        },
-        images: []
+        }
   }
-   if (req.files) {
-        newJob.images.push({
-            fieldname: req.files.fieldname,
-            filename: req.files.filename,
-            originalname: req.files.originalname,
-            path: req.files.path
-        })
-    }
+
+
+   
   Job.findByIdAndUpdate(findJob, change)
     .then((response)=>{
+      debugger
       res.json({message: response});
     })
     .catch(error=>{
@@ -44,27 +38,39 @@ router.post('/editJob', (req, res, next)=>{
     })
 })
 
-// router.post('/editprofilepicture/:userid', uploadCloud.single('profilePicture'), (req, res, next)=>{
-//   debugger
+router.post('/editJob/images', uploadCloud.array('images'), (req, res, next)=>{
+  debugger
 
-//   const findUser = req.params.userid;
-//   const change = {
-//     profilePicture: {
-//       fieldname: req.file.fieldname,
-//       filename: req.file.filename,
-//       originalname: req.file.originalname,
-//       path: req.file.path
-//     }
-//   }
+  const findJob = req.body.jobId;
 
-//   User.findByIdAndUpdate(findUser, change)
-//     .then((response)=>{
-//         res.json({message: response});
-//     })
-//     .catch(error=>{
-//       res.json({error: error});
-//       console.log(error, "Error updating profile picture");
-//     })
-// })
+  let images = []
+  if (req.files) {
+    debugger
+   req.files.forEach(file => {
+        images.push({
+           fieldname: file.fieldname,
+           filename: file.filename,
+           originalname: file.originalname,
+           path: file.path
+       })
+     })
+   }
+
+  let change = {
+        images: images
+  }
+
+
+   
+  Job.findByIdAndUpdate(findJob, change)
+    .then((response)=>{
+      debugger
+      res.json({message: response});
+    })
+    .catch(error=>{
+      res.json({error: error});
+      console.log(error, "Error updating userprofile");
+    })
+})
 
 module.exports = router;
