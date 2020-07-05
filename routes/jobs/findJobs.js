371e361124ce6917b01618cda90f1f6d49a23e95
Nardo Kraaijeveld <nardo.kra@gmail.com>
2020-07-05 17:23:36 +0200

    added find one specific job route

--- routes/jobs/findJobs.js
+++ routes/jobs/findJobs.js
@@ -2,15 +2,25 @@ const express = require('express');
 const router  = express.Router();
 const Job = require('../../models/Job')
 
-router.get('/findJobs', (req, res, next) =>{
-  debugger
-    Job
-        .find({})
-        .then((allJobs)=>{
-          res.json(allJobs);
-        })
-        .catch(error =>{
-          console.log('This is the invalid field ->', error)
-      })
+router.get('/findjobs', (req, res, next) =>{
+  Job.find({})
+    .then((allJobs)=>{
+      res.json(allJobs);
+    })
+    .catch(error =>{
+      console.log('This is the invalid field ->', error)
+  })
 })
+
+router.get('/findjob/:id', (req, res, next) =>{
+  Job.findById(req.params.id)
+    .then((job)=>{
+      res.json(job);
+    })
+    .catch(error =>{
+      console.log('This is the invalid field ->', error);
+      res.json(error.message);
+    })
+})
+
 module.exports = router;
