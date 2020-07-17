554dd434f31760e7cbce6151dd4c7fd195277f96
Elko Lemiso <elkovandongen@gmail.com>
2020-07-17 15:23:27 +0200

    CORS fix

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,6 @@ var express = require('express');
 var router = express.Router();
 // var path = require('path');
 
-router.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
